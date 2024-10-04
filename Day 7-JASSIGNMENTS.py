9ac53ee81e7ae7dddf0643a0997e874442d47a08
Anant Rathore 025 <145332995+anantrathore025@users.noreply.github.com>
2024-10-04 09:57:38 +0530

    Create Day 7-JASSIGNMENTS.py

---
+++ Day 7-JASSIGNMENTS.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+
+
+for _ in range(T):
+
+    X = int(input())
+
+    finish_time = X + 3
+
+    if finish_time <= 10: 
+
+        print("Yes") 
+
+    else:
+
+        print("No")  
+
+        
