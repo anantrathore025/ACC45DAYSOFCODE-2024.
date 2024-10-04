9c5f0f016a86dd65a4d8a097fff020859e73b1e8
Anant Rathore 025 <145332995+anantrathore025@users.noreply.github.com>
2024-10-04 10:01:25 +0530

    Add files via upload

--- Day 7-JASSIGNMENTS.py
+++ Day 7-JASSIGNMENTS.py
@@ -1,19 +1,10 @@
 T = int(input())
 
-
-
 for _ in range(T):
-
     X = int(input())
-
     finish_time = X + 3
-
     if finish_time <= 10: 
-
         print("Yes") 
-
     else:
-
         print("No")  
-
         
